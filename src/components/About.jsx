432c0abd8d9531174c9d4bb8059e56ecf0636e30
ShafiAmirullaShaik <esoft1047@gmail.com>
2025-04-27 11:02:19 +0530

    new changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 // import aboutImg from '../assets/about.jpg';
 import { ABOUT_TEXT } from "../constants";
-import { BsPersonVideo3 } from "react-icons/bs";
 import { motion } from "framer-motion";
 import { useLightDarkMode } from '../context/LightDarkMode';
 
